c15451ac131a4a3af8ee0292315733450b694abb
Tim Needham <tim.needham@wmfs.net>
2018-04-18 12:14:27 +0100

    Adds locally-served fonts.

--- src/components/everest.vue
+++ src/components/everest.vue
@@ -137,17 +137,17 @@ export default {
 
   @font-face {
     font-family: 'Baloo Thambi';
-    src: url('/static/fonts/BalooThambi-Regular.ttf');
+    src: url('/virtual-everest-climb/static/fonts/BalooThambi-Regular.ttf');
   }
 
   @font-face {
     font-family: 'Oxygen Mono';
-    src: url('/static/fonts/OxygenMono-Regular.ttf');
+    src: url('/virtual-everest-climb/static/fonts/OxygenMono-Regular.ttf');
   }
 
   @font-face {
     font-family: 'Jaldi';
-    src: url('/static/fonts/Jaldi-Regular.ttf');
+    src: url('/virtual-everest-climb/static/fonts/Jaldi-Regular.ttf');
   }
 
   h1 {
